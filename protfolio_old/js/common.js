54c70226b0d7497cf307057330667bc9bcbcf707
MMichotte <martin.michotte@gmail.com>
2020-10-25 11:21:38 +0100

    removed old version

--- protfolio_old/js/common.js
+++
@@ -1,8 +0,0 @@
-
-
-function pageInit(){
-    let page = window.location.pathname.split("/").pop().split(".").shift();
-    document.getElementById(page).style.color="rgba(38, 163, 235, 0.836)";
-    document.getElementById(page).style.textDecoration="underline";
-    document.getElementById(page).style.fontWeight="bold";
-}
